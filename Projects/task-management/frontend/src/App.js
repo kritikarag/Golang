6c3a3ac7795595799a3ba56a1e832991df59ba85
Kritika Rag <kritikarag.kr@gmail.com>
2023-07-03 05:41:27 +0530

    dELETED NODE MODULES

--- Projects/task-management/frontend/src/App.js
+++ Projects/task-management/frontend/src/App.js
@@ -1,32 +1,14 @@
-import React from 'react';
+import React from "react";
 //import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import ListTaskComponent from './components/ListTaskComponent';
-import HeaderComponent from './components/HeaderComponent';
-import FooterComponent from './components/FooterComponent';
-import CreateTaskComponent from './components/CreateTaskComponent';
-import ViewTaskComponent from './components/ViewTaskComponent';
-
+import { Container } from 'semantic-ui-react';
+import TaskManagement from './task_management';
 function App() {
   return (
     <div>
-        <Router>
-              <HeaderComponent />
-                <div className="container">
-                    <Routes> 
-                          <Route path = "/" exact component =
-                              {ListTaskComponent}></Route>
-                          <Route path = "/Tasks" component = 
-                              {ListTaskComponent}></Route>
-                          <Route path = "/add-Task/:id" component = 
-                              {CreateTaskComponent}></Route>
-                         <Route path = "/view-Task/:id" component = 
-                              {ViewTaskComponent}></Route>
-                         </Routes>
-                </div>
-              <FooterComponent />
-        </Router>
+         <Container>
+          <TaskManagement/>
+         </Container>
     </div>
     
   );
